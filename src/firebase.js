246d6eb4353821c23983d266613437012cbb04e5
stanislaukostka <zelasikas@gmail.com>
2022-07-11 17:32:18 +0300

    fix firebase first #57

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-    // databaseURL: "https://healtuapp-fit-default-rtdb.europe-west1.firebasedatabase.app/",
     apiKey: "AIzaSyBhXqi3fQB9DD4g6EjNs0YfXDxfC8r6XM4",
     authDomain: "healtuapp-fit.firebaseapp.com",
     projectId: "healtuapp-fit",
