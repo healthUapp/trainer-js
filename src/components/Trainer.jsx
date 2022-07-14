936167fb39f0c554f947fe2e4e82386cfac0d1ec
Dmitriy <dmitrivanich@gmail.com>
2022-07-14 14:20:42 +0300

    ,

--- src/components/Trainer.jsx
+++ src/components/Trainer.jsx
@@ -25,7 +25,7 @@ export default function Trainer({visibleBody, dots, selectedCource, set, allSets
     const [time, setTime] = useState(set[exerciseNumber].time)
     const [pause, setPause] = useState(5)
     const [selectedGif,setSelectedGif] = useState(allExercises[set[exerciseNumber].exerciseIndex].gif)
-    const [showResults, setShowResults] = useState(false)
+    const [showResults, setShowResults] = useState(true)
     const [results, setResults] = useState([])
     
 
